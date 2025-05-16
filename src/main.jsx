95bdf0e3d306df966d962074f685a78c63dda459
Bibster3 <bilyana.f.stefanova@gmail.com>
2025-05-16 12:04:00 +0300

    Major changes to structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; 
-import { gsap } from 'gsap';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
