fe62463b68cc29b5fb8af3d4a2c867fc0d9ef52d
Bibster3 <bilyana.f.stefanova@gmail.com>
2025-06-02 10:39:14 +0300

    Lazy Loading

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+
 import ProjectCard from './ProjectCard';
 
 const Portfolio = () => {
@@ -9,26 +10,57 @@ const Portfolio = () => {
             iframeUrl: 'https://bibster3.github.io/CalorieMate/', 
             githubUrl: 'https://github.com/Bibster3/CalorieMate'
         },
-      /*  {
-            title: 'Project 2',
-            description: 'Description of Project 2',
-            iframeUrl: 'https://example.com/project2', 
-            githubUrl: 'https://github.com/Bibster3/project2'
+       {
+            title: 'WeatherApp',
+            description: 'Weatherly is a modern web application designed to keep you informed about the weather conditions in your area and beyond. Built with React, TypeScript, and Tailwind CSS, Weatherly offers a seamless user experience with real-time updates and intuitive navigation.',
+            iframeUrl: 'https://bibster3.github.io/WeatherApp/', 
+            githubUrl: 'https://github.com/Bibster3/WeatherApp'
         }
         //more projects to be added */
     ];
+
+  const [visibleProjects, setVisibleProjects] = useState([]);
+    const projectRefs = useRef([]);
+
+    // IntersectionObserver callback function to check if the project is in view
+    const onIntersection = (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const projectIndex = entry.target.getAttribute('data-index');
+                setVisibleProjects((prevVisibleProjects) => [...prevVisibleProjects, Number(projectIndex)]);
+            }
+        });
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(onIntersection, {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1 // Adjust as needed based on your layout and visibility needs
+        });
+
+        projectRefs.current.forEach((ref) => {
+            observer.observe(ref);
+        });
+
+        return () => observer.disconnect();
+    }, []);
 
     return (
         <div className="portfolio">
             <div className="project-list">
                 {projects.map((project, index) => (
-                    <ProjectCard
-                        key={index}
-                        title={project.title}
-                        description={project.description}
-                        iframeUrl={project.iframeUrl}
-                        githubUrl={project.githubUrl}
-                    />
+                    <div key={index} ref={(ref) => (projectRefs.current[index] = ref)} data-index={index}>
+                        {visibleProjects.includes(index) && (
+                            <ProjectCard
+                                key={index}
+                                title={project.title}
+                                description={project.description}
+                                iframeUrl={project.iframeUrl}
+                                githubUrl={project.githubUrl}
+                            />
+                        )}
+                    </div>
                 ))}
             </div>
         </div>
